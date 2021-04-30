426898cc3a7970fa3c43f55c572422e645e76238
DmitriyNikolaevich <dnkaplun1987@gmail.com>
2021-04-30 07:12:24 +0300

    29042021 evening 2

---
+++ src/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3500
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+// app.use(cors())
+app.options('*', cors())
+
+const routes = require('./settings/routes')
+routes(app)
+
+
+app.listen(port, () => {
+    console.log(`Server listen on port ${port}`)
+})
